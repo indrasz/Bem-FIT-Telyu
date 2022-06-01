4cfb3abc014c6959d0f9d037710e2a2ba77af785
wahyuindra <wahyuindra712@gmail.com>
2022-06-01 13:27:16 +0700

    convert vlade template dashboard

---
+++ resources/views/includes/admin/style.blade.php
@@ -0,0 +1,10 @@
+<!--     Fonts and icons     -->
+<link href="{{ url('https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700') }}" rel="stylesheet" />
+<!-- Nucleo Icons -->
+<link href="{{ asset('admin/assets/css/nucleo-icons.css') }}" rel="stylesheet" />
+<link href="{{ asset('admin/assets/css/nucleo-svg.css') }}" rel="stylesheet" />
+<!-- Font Awesome Icons -->
+<script src="{{ url('https://kit.fontawesome.com/42d5adcbca.js') }}" crossorigin="anonymous"></script>
+<link href="{{ asset('admin/assets/css/nucleo-svg.css') }}" rel="stylesheet" />
+<!-- CSS Files -->
+<link id="pagestyle" href="{{ asset('admin/assets/css/soft-ui-dashboard.css?v=1.0.5') }}" rel="stylesheet" />
